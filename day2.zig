c3b85fd291cb7da8b8278187a63477c8c5c47a9e
ec965 <enoch965@gmail.com>
2022-12-20 00:18:26 -0800

    d2 p1

---
+++ day2.zig
@@ -0,0 +1,52 @@
+const std = @import("std");
+const fmt = @import("std").fmt;
+const print = @import("std").debug.print;
+
+// have to preprocess file and add extra comma at end of file
+const input_file = "./inputs/day2.txt";
+
+fn print_parts(parts: *[1024]u32, len: u32) void {
+    var i: u32 = 0;
+    while (i < len) {
+        print("{d} ", .{parts[i]});
+        i += 1;
+    }
+    print("\n", .{});
+}
+
+pub fn main() !void {
+    var file = try std.fs.cwd().openFile(input_file, .{});
+    var buf_reader = std.io.bufferedReader(file.reader());
+    var in_stream = buf_reader.reader();
+
+    var parts: [1024]u32 = undefined;
+
+    var buf: [1024]u8 = undefined;
+
+    var len: u32 = 0;
+    while (try in_stream.readUntilDelimiterOrEof(&buf, ',')) |part| {
+        const res = fmt.parseInt(u32, part, 10) catch null;
+        if (res != null) {
+            parts[len] = res.?;
+            len += 1;
+        }
+    }
+
+    parts[1] = 12;
+    parts[2] = 2;
+
+    var i: u32 = 0;
+    while (i < len) {
+        if (parts[i] == 1) {
+            parts[parts[i + 3]] = parts[parts[i + 1]] + parts[parts[i + 2]];
+        } else if (parts[i] == 2) {
+            parts[parts[i + 3]] = parts[parts[i + 1]] * parts[parts[i + 2]];
+        } else if (parts[i] == 99) {
+            break;
+        }
+
+        i += 4;
+    }
+
+    print("part1: {d}\n", .{parts[0]});
+}
